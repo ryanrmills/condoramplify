46eabf835c71108fd3314cb347873706891db1be
ryanrmills <130598044+ryanrmills@users.noreply.github.com>
2023-06-03 14:01:35 -0700

    Add files via upload

---
+++ condorAmplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "condoramplify97851b02": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
